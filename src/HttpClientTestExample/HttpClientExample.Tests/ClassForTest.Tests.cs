4276869e10f3654532463c92db4a2af314390b24
Nikita Tulenev <n1tulenev@gmail.com>
2021-02-27 11:04:50 +0300

    Add test project

---
+++ src/HttpClientTestExample/HttpClientExample.Tests/ClassForTest.Tests.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Net.Http;
+
+using FluentAssertions;
+
+using Xunit;
+
+using HttpClientTestExample;
+using Moq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpClientExample.Tests
+{
+    public class ExampleTests
+    {
+        /// <summary>
+        /// HttpMessageHandler mock
+        /// </summary>
+        public class MockHttpMessageHandler : HttpMessageHandler
+        {
+            public MockHttpMessageHandler(string content, string method, string uri)
+            {
+                _content = content;
+                _method = method;
+                _uri = uri;
+            }
+
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+                                                                   CancellationToken cancellationToken)
+            {
+                if (request.RequestUri!.ToString() == _uri && request.Method.ToString() == _method)
+                {
+                    return Task.FromResult(new HttpResponseMessage
+                    {
+                        Content = new StringContent(_content)
+                    });
+                }
+                else
+                {
+                    throw new InvalidOperationException("Invalid params");
+                }
+            }
+
+            private readonly string _content;
+            private readonly string _uri;
+            private readonly string _method;
+        }
+
+        [Fact(DisplayName = "Web request should return correct content length.")]
+        [Trait("Category", "Unit")]
+        public async Task CanReadContentWithValidParamsAsync()
+        {
+            // Arrange
+            var testString = "test123";
+            var uri = "https://www.test.com/";
+            var handler = new MockHttpMessageHandler(testString, "GET", uri);
+            var client = new HttpClient(handler);
+            var testClass = new ClassForTest(client);
+
+            var result = 0;
+            // Act
+            var exception = await Record.ExceptionAsync(
+                async () => result = await testClass.GetContentLengthAsync(uri, CancellationToken.None));
+
+            // Assert
+            exception.Should().BeNull();
+            result.Should().Be(testString.Length);
+
+        }
+
+        [Fact(DisplayName = "ClassForTest can't be constructed with null HttpClient.")]
+        [Trait("Category", "Unit")]
+        public void CantBeConstructedNullClient()
+        {
+            // Arrange
+            HttpClient client = null!;
+
+            // Act
+            var exception = Record.Exception(
+                () => new ClassForTest(client));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Fact(DisplayName = "ClassForTest can't be constructed with null HttpClient.")]
+        [Trait("Category", "Unit")]
+        public void CanBeConstructedValidClient()
+        {
+            // Arrange
+            var handlerMoq = new Mock<HttpMessageHandler>();
+            HttpClient client = new HttpClient(handlerMoq.Object);
+
+            // Act
+            var exception = Record.Exception(
+                () => new ClassForTest(client));
+
+            // Assert
+            exception.Should().BeNull();
+        }
+
+        [Fact(DisplayName = "ClassForTest can't send message for null uri.")]
+        [Trait("Category", "Unit")]
+        public async Task CantSendMessageOnNullUriAsync()
+        {
+            // Arrange
+            var handlerMoq = new Mock<HttpMessageHandler>();
+            HttpClient client = new HttpClient(handlerMoq.Object);
+            var testClass = new ClassForTest(client);
+            string uri = null!;
+
+            // Act
+            var exception = await Record.ExceptionAsync(
+                async () => await testClass.GetContentLengthAsync(uri, CancellationToken.None));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Fact(DisplayName = "ClassForTest can't send message for empty uri.")]
+        [Trait("Category", "Unit")]
+        public async Task CantSendMessageOnEmptyUriAsync()
+        {
+            // Arrange
+            var handlerMoq = new Mock<HttpMessageHandler>();
+            HttpClient client = new HttpClient(handlerMoq.Object);
+            var testClass = new ClassForTest(client);
+            string uri = string.Empty;
+
+            // Act
+            var exception = await Record.ExceptionAsync(
+                async () => await testClass.GetContentLengthAsync(uri, CancellationToken.None));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+        }
+
+        [Fact(DisplayName = "ClassForTest can't send message for uri of whitespaces.")]
+        [Trait("Category", "Unit")]
+        public async Task CantSendMessageOnAllWhitespacesUriAsync()
+        {
+            // Arrange
+            var handlerMoq = new Mock<HttpMessageHandler>();
+            HttpClient client = new HttpClient(handlerMoq.Object);
+            var testClass = new ClassForTest(client);
+            string uri = "     ";
+
+            // Act
+            var exception = await Record.ExceptionAsync(
+                async () => await testClass.GetContentLengthAsync(uri, CancellationToken.None));
+
+            // Assert
+            exception.Should().NotBeNull().And.BeOfType<ArgumentException>();
+        }
+    }
+}
---
+++ src/HttpClientTestExample/HttpClientTestExample/ClassForTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpClientTestExample
+{
+    /// <summary>
+    /// Class for example of test with mocking of http client.
+    /// </summary>
+    public class ClassForTest
+    {
+        public ClassForTest(HttpClient httpClient)
+        {
+            if (httpClient == null)
+            {
+                throw new ArgumentNullException(nameof(httpClient));
+            }
+
+            _client = httpClient;
+        }
+
+        /// <summary>
+        /// Calculated web resource string length.
+        /// </summary>
+        /// <param name="uri">web resource.</param>
+        /// <param name="ct">token.</param>
+        /// <returns>length of content.</returns>
+        public async Task<int> GetContentLengthAsync(string uri, CancellationToken ct)
+        {
+            if (string.IsNullOrEmpty(uri))
+            {
+                throw new ArgumentNullException(nameof(uri));
+            }
+
+            if (string.IsNullOrWhiteSpace(uri))
+            {
+                throw new ArgumentException("Uri can't contains only whitespaces.", nameof(uri));
+            }
+
+            var data = await _client.GetStringAsync(uri, ct);
+
+            return data.Length;
+        }
+
+        private HttpClient _client;
+    }
+}
